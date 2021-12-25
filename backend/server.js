000425d83571c4ea813e35f4f49ad1823b581bea
Dalal-12 <dalal.cs@hotmail.com>
2021-12-25 09:46:23 -0800

    finish put and delete

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,32 @@ Todo.create(req.body,(err,newTask)=>{
 if(err){console.log("ERROR: ",err)}
 else{res.status(201).json(newTask)}
 })
- 
+})
+
+server.delete("/tasks/:id",(req,res)=>{
+    Todo.deleteOne({_id:req.params.id},(err,deleteObj)=>{
+        if(err){console.log("ERROR: ",err)
+    }
+        else{
+            
+            deleteObj.deletedCount===1
+             ? res.json("Deleted todo successfully")
+            : res.status(404).json("This todo is not found")
+        }
+    })
+})
+
+server.put("/tasks/:id",(req,res)=>{
+    Todo.updateOne({_id:req.params.id},{title:req.body.newTitle},(err,updateObj)=>{
+        if(err){console.log("ERROR: ",err)
+        res.status(400).json(err)
+    }else{
+        console.log(updateObj)
+        updateObj.modifiedCount===1
+        ? res.json("Update one todo successfully")
+        : res.status(404).json("This todo is not found")
+    }
+    })
 })
 
 server.listen(5000,()=>{console.log("Server is On...")})
