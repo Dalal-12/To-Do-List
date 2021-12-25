319111205247d0e984f7961b03164e5ecbeaed93
Dalal-12 <dalal.cs@hotmail.com>
2021-12-25 03:55:54 -0800

    finish get and post

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+const { application } = require('express')
 const express=require('express')
 const server=express()
 const db=require('./db')
 const Todo=require('./models/todo')
+server.use(express.json())
 console.log(Todo)
 
 
@@ -11,11 +13,18 @@ res.json('GET  is working')
 })
 
 server.get("/tasks",(req,res)=>{
-    res.json('GET  is working')
+    Todo.find({},(err,data)=>{
+        if(err){console.log("ERROR: ",err)}else{res.json(data)}
+    })
     })
 
-server.post("/",(req,res)=>{
-    console.log("Create New Todo Successfully")
+
+server.post("/tasks",(req,res)=>{
+Todo.create(req.body,(err,newTask)=>{
+if(err){console.log("ERROR: ",err)}
+else{res.status(201).json(newTask)}
+})
+ 
 })
 
 server.listen(5000,()=>{console.log("Server is On...")})
