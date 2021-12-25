a356b7e228cef54a395035429de552535c5bf0fd
Dalal-12 <dalal.cs@hotmail.com>
2021-12-25 13:37:06 -0800

    finish Delete all completed and update status

--- backend/server.js
+++ backend/server.js
@@ -62,4 +62,34 @@ server.get("/filter",(req,res)=>{
     })
 })
 
+server.delete("/tasks",(req,res)=>{
+    Todo.deleteMany({isCompleted:true},(err,deleteObj)=>{
+        if(err){console.log("ERROR: ",err)
+    }
+        else{
+            console.log(deleteObj)
+            deleteObj.deletedCount===0
+            ?res.status(404).json("There is no completed todo  found")
+            : res.json("Deleted all completed todo successfully")
+            
+        }
+    })
+})
+
+
+server.put("/tasks/:id/:isCompleted",(req,res)=>{
+    console.log("124:",req.params)
+    Todo.updateOne({_id:req.params.id},{isCompleted:req.params.isCompleted},
+        (err,updateObj)=>{
+        if(err){console.log("ERROR: ",err)
+        res.status(400).json(err)
+    }else{
+        console.log(updateObj)
+        updateObj.modifiedCount===1
+        ? res.json("Update one todo successfully")
+        : res.status(404).json("This todo is not found")
+    }
+    })
+})
+
 server.listen(5000,()=>{console.log("Server is On...")})
